ba2a53a5f7a23460794bb778065543713037c25e
Noe Lambert <nlambert@bess-f3r2s10.clusters.42paris.fr>
2025-03-11 14:47:49 +0100

    parse map

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,12 +6,23 @@
 
 # include "../libft/libft.h"
 # include "../minilibx-linux/mlx.h"
-#include <stdlib.h>
+# include <stdlib.h>
+
+typedef struct s_position {
+	int	x;
+	int	y;
+}	t_position;
 
 typedef struct s_data
 {
-    void *mlx;
-    void *win_ptr;
-}   t_data;
+	void			*mlx;
+	void			*win_ptr;
+	char			**map;
+	t_position		map_size;
+}	t_data;
+
+
+int	check_map(t_data *data);
+int	check_sides(char **map, int x, int y, t_position map_size);
 
 #	endif
--- srcs/map_parsing.c
+++ srcs/map_parsing.c
@@ -3,10 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   map_parsing.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hugmonch <hugmonch@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nlambert <nlambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 12:21:58 by nlambert          #+#    #+#             */
-/*   Updated: 2025/03/11 14:15:58 by hugmonch         ###   ########.fr       */
+/*   Updated: 2025/03/11 14:46:01 by nlambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/cub3d.h"
+
+
+int	check_sides(char **map, int x, int y, t_intvector map_size)
+{
+	if (x == 0 || y == 0 || x == map_size.x - 1 || y == map_size.y - 1)
+		return (false);
+	if (map[y - 1][x] == ' ' || map[y][x - 1] == ' '
+			|| map[y + 1][x] == ' ' || map[y][x + 1] == ' ')
+		return (false);
+	return (true);
+}
+/*
+	parse les orientations de la map, check si elle est closed
+*/
+int	check_map(t_data *data)
+{
+	char	**map;
+	int		x;
+	int		y;
+
+	map = data->map;
+	y = 0;
+	while (y < data->map_size.y)
+	{
+		x = 0;
+		while (x < data->map_size.x)
+		{
+			if (map[y][x] != '0' && map[y][x] != '1' && map[y][x] != 'N'
+				&& map[y][x] != 'W' && map[y][x] != 'S' && map[y][x] != 'E'
+				&& map[y][x] != ' ')
+				return (printf("Unknown character in map.\n", 1), 0);
+			if (map[y][x] == '0')
+			{
+				if (!_check_sides(map, x, y, data->map_size))
+					return (printf("Map must be closed.\n", 1), 0);
+			}
+			x++;
+		}
+		y++;
+	}
+	return (1);
+}
